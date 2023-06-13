6b0fd7d5e9ffacdeeae0333ce022595270295650
Ajit-Dwivedi <118682525+Ajit-Dwivedi@users.noreply.github.com>
2023-06-13 11:37:09 +0530

    App.js

--- Ajit Dwivedi- 220940120022/Kratin Healthcare/src/App.js
+++ Ajit Dwivedi- 220940120022/Kratin Healthcare/src/App.js
@@ -1,8 +1,5 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-//import CustomerHomePage from "./Pages/customerHomePage/CustomerHomePage";
 import Home from "./Pages/home/Home";
-//import Hotel from "./Pages/hotel/Hotel";
-//import List from "./Pages/list/List";
 import Login from "./Pages/login/Login";
 import SignUp from "./Pages/userRegister/SignUp";
 function App() {
@@ -12,15 +9,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/register" element={<SignUp/>}></Route>
-
-
-        {/* <Route path="/hotels" element={<List/>}></Route>
-        <Route path="/hotels/:id" element={<Hotel/>}></Route>
-        
-        <Route path="/customer" element={<CustomerHomePage />}></Route> */}
-        
-
-      </Routes>
+        </Routes>
     </BrowserRouter>
   )
 }
